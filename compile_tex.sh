8e3a84d902a50bcf151e62d868d64c445e5de55f
Derek Huang <djh458@stern.nyu.edu>
2021-02-08 00:27:54 -0500

    update compile_tex.sh paths and usage

--- compile_tex.sh
+++ compile_tex.sh
@@ -8,8 +8,9 @@ PDF_TEX="pdflatex -interaction=nonstopmode -halt-on-error -shell-escape"
 # script usage
 USAGE="usage: $0 [-h] [TEXFILE]
 
-For each .tex file in each lecture_[0-9]+ directory in lectures, compiles the
-.tex file with pdflatex and writes the output to the same directory the
+For each .tex file in the top-level package directory and for each
+lecture_[0-9]+ directory located in the top-level package directory, compiles
+the .tex file with pdflatex and writes the output to the same directory the
 respective .tex file is located in. The pdflatex command used is
 
 $PDF_TEX
@@ -21,23 +22,30 @@ provided .tex source file resides in.
 optional arguments:
  -h, --help  show this usage
  TEXFILE     specified .tex file to compile, writing output to the same
-             directory the source file is located in. if omitted, then the .tex
-             files in each lecture_[0-9]+ directory in lectures are compiled."
+             directory the source file is located in. if omitted, then all .tex
+             files in the top-level package directory and each lecture_[0-9]+
+             directory within in the top-level package directory are compiled."
 # directory this file is located in, the repo root (relative path)
 REPO_ROOT=$(dirname $0)
 
 # if no arguments, then compile all lectures
 if [[ $# == 0 ]]
 then
-    # for each lecture directory in the lectures directory
-    for LEC_DIR in $REPO_ROOT/lectures/lecture*
+    # for all .tex files in the top-level package directory
+    for INFILE in $REPO_ROOT/bac_advanced_ml/*.tex
+    do
+        # note redirect to /dev/null doesn't include 2>&1 so output from
+        # stderr will still be shown. echo + sed used to replace .tex with
+        # empty string for the job name.
+        $PDF_TEX -jobname="$(echo $INFILE | sed s/.tex//g)" $INFILE > /dev/null
+    done
+    # for each lecture directory in the package directory
+    for LEC_DIR in $REPO_ROOT/bac_advanced_ml/lecture*
     do
         # for each .tex file in the directory
         for INFILE in $LEC_DIR/*.tex
         do
-            # note redirect to /dev/null doesn't include 2>&1 so output from
-            # stderr will still be shown. echo + sed used to replace .tex with
-            # empty string for the job name.
+            # use same compilation command as above
             $PDF_TEX -jobname="$(echo $INFILE | sed s/.tex//g)" $INFILE \
                 > /dev/null
         done
