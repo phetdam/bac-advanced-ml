930311178231ebeffe8deffa99ce7570cfcc81e3
Derek Huang <djh458@stern.nyu.edu>
2021-03-07 19:30:53 -0500

    update conftest.py with blobcls fixture for easy linear classification problem

--- bac_advanced_ml/models/tests/conftest.py
+++ bac_advanced_ml/models/tests/conftest.py
@@ -1,7 +1,8 @@
 __doc__ = "Global fixtures for model testing."
 
+import numpy as np
 import pytest
-from sklearn.datasets import make_regression
+from sklearn.datasets import make_blobs, make_regression
 from sklearn.model_selection import train_test_split
 
 
@@ -31,11 +32,18 @@ def linreg(global_seed):
 
     Returns
     -------
-    tuple
-        X_train, X_test, y_train, y_test, coef, bias. X_train has shape
-        (480, 10), X_test has shape (120, 10), y_train has shape (480,), y_test
-        has shape (120,), the true coefficients coef has shape (10,), while the
-        true intercept bias is a float.
+    X_train : numpy.ndarray
+        Training input data, shape (480, 10)
+    X_test : numpy.ndarray
+        Test/validation input data, shape (120, 10)
+    y_train : numpy.ndarray
+        Training response vector, shape (480,)
+    y_test : numpy.ndarray
+        Test/validation response vector, shape (120,)
+    coef : numpy.ndarray
+        True model coefficients, shape (10,)
+    bias : float
+        True model bias (intercept)
     """
     # intercept for the regression problem
     bias = 7
@@ -50,3 +58,52 @@ def linreg(global_seed):
     )
     # return split data, coef, and bias
     return X_train, X_test, y_train, y_test, coef, bias
+
+
+@pytest.fixture(scope = "session")
+def blobcls(global_seed):
+    """Generated blob classification problem with train/test split.
+
+    Uses sklearn.datasets.make_blobs to make isotropic standard multivariate
+    Gaussian blobs in 10-dimensional Euclidean space with centers
+
+    .. code::
+
+       np.array([-4, -2, -5, 1, -6, -2, -7, 2, -5, 1])
+       np.array([6, 5, 3, 4, 7, -1, 6, 1, -2, 9])
+
+    The returned data has 600 samples, 10 features. Features are informative by
+    construction so we don't use sklearn.datasets.make_classification.
+
+    Parameters
+    ----------
+    global_seed : int
+        pytest fixture. See conftest.py
+
+    Returns
+    -------
+    X_train : numpy.ndarray
+        Training input data, shape (480, 10)
+    X_test : numpy.ndarray
+        Test/validation input data, shape (120, 10)
+    y_train : numpy.ndarray
+        Training class labels, shape (480,)
+    y_test : numpy.ndarray
+        Test/validation class labels, shape (120,)
+    """
+    # centers for the two data clusters
+    centers = np.array(
+        [[-4, -2, -5, 1, -6, -2, -7, 2, -5, 1],
+         [6, 5, 3, 4, 7, -1, 6, 1, -2, 9]]
+    )
+    # generate noisy classification problem using isotropic Gaussian blobs
+    X, y = make_blobs(
+        n_samples = 600, n_features = 10, centers = centers,
+        random_state = global_seed
+    )
+    # split the data with train_test_split and return
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size = 0.2, random_state = global_seed
+    )
+    # return split data, coef, and bias
+    return X_train, X_test, y_train, y_test
